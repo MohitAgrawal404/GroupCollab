c872ff139058d04eeca6a008b00f8b4b32b45657
Mohit Agrawal <mohit@mohits-air.lan>
2022-08-16 22:10:52 -0700

    adding login template

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import {auth, provider} from '../backend/firebase';
+import {signInWithPopup} from "firebase/auth";
+import {useNavigate} from "react-router-dom";
 
-export const Login = () => {
-  return (
-    <div>
-      <h1>THIS IS LOGIN</h1>
-    </div>
-  )
+export const Login = ({setIsAuth}) => {
+    let navigate = useNavigate();
+    
+    const signInWithGoogle = () => {
+        signInWithPopup(auth, provider).then(() => {
+            localStorage.setItem("isAuth", true);
+            setIsAuth(true);
+            navigate("/dashboard");
+        });
+    };
+
+    return (
+        <div className = "App">
+            <h1 className="title">Sign In / Sign Up: </h1>
+            <button className="delete" onClick={signInWithGoogle}>
+                Sign in with Google
+            </button>
+        </div>
+    )
 }
