17b09d9ec7192dfc98efdc3902d0415283d5a580
Josh <joshmiranda.exe@gmail.com>
2022-08-16 20:25:32 -0400

    routes

--- client/src/backend/firebase.js
+++ client/src/backend/firebase.js
@@ -1,11 +1,17 @@
 import firebase from "firebase/compat/app";
 import "firebase/compat/firestore";
 import { initializeApp } from "firebase/app";
-import {getFirestore} from 'firebase/firestore';
-import {getAuth, GoogleAuthProvider} from 'firebase/auth'
+import { getFirestore } from "firebase/firestore";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  //ASK FOR DETAILS
+  apiKey: "AIzaSyB0Px6dHZzp8U2PiqSME8vIBG9Zr69gtnQ",
+  authDomain: "gcollab-109ba.firebaseapp.com",
+  projectId: "gcollab-109ba",
+  storageBucket: "gcollab-109ba.appspot.com",
+  messagingSenderId: "871201771773",
+  appId: "1:871201771773:web:63dd9d5b0307b66e4a2e94",
+  measurementId: "G-Y1QGH1C4TS",
 };
 
 if (!firebase.apps.length) {
@@ -13,6 +19,7 @@ if (!firebase.apps.length) {
 }
 
 export const firestore = firebase.firestore();
-export const db = getFirestore(app);
-export const auth = getAuth(app);
+export const db = getFirestore(firebaseConfig);
+export const auth = getAuth(firebaseConfig);
 export const provider = new GoogleAuthProvider();
+export default firebaseConfig;
---
+++ client/src/pages/Meetingroom.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Meetingroom = () => {
+  return (
+    <div>Meetingroom</div>
+  )
+}
