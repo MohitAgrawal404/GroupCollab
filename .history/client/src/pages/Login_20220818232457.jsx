043c4837a9cd955af932f121f531e61983e4bc0e
Victor <victorjosuepimentel21@gmail.com>
2022-08-18 23:32:22 -0400

    Added Login

--- .history/client/src/pages/Login_20220818232457.jsx
+++ .history/client/src/pages/Login_20220818232457.jsx
@@ -54,10 +54,7 @@ export const Login = ({ setIsAuth }) => {
     <div class="max-w-lg mx-auto my-10 bg-white p-8 rounded-xl shadow shadow-slate-300 ">
       <h1 class="text-4xl font-medium">Login</h1>
       <div class="my-5">
-        <button
-          class="w-full text-center py-3 my-3 border flex space-x-2 items-center justify-center border-slate-200 rounded-lg text-slate-700 hover:border-slate-400 hover:text-slate-900 hover:shadow transition duration-150"
-          onClick={signInWithGoogle}
-        >
+        <button class="w-full text-center py-3 my-3 border flex space-x-2 items-center justify-center border-slate-200 rounded-lg text-slate-700 hover:border-slate-400 hover:text-slate-900 hover:shadow transition duration-150">
           <img
             src="https://www.svgrepo.com/show/355037/google.svg"
             class="w-6 h-6"
